9fdea71a989159bd4a4808797fc95b9951b417e8
Guna Vardhan <varivardhan15@gmail.com>
2025-07-31 17:02:09 +0530

    feat: create typing indiactor component

---
+++ src/components/TypingIndicator.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const TypingIndicator: React.FC = () => (
+  <div className="self-start italic text-gray-500 dark:text-gray-400 mb-2">
+    Bot is typing...
+  </div>
+);
+
+export default TypingIndicator;
